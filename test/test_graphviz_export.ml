ed5eb633c56cde0de67511a3030c76c1457c0304
Eric <ec_936@cornell.edu>
2026-04-28 13:19:00 -0400

    Added test suite for graphviz
    -Tested .dot file writing behavior with terminal
    -Bisect coverage ~80%
    -Added *.dot to .gitignore

---
+++ test/test_graphviz_export.ml
@@ -0,0 +1,77 @@
+open OUnit2
+open Ocaml_autodiff.Graphviz_export
+open Ocaml_autodiff.Expr
+
+let count_occurrences str sub =
+  let sub_len = String.length sub in
+  let str_len = String.length str in
+  let count = ref 0 in
+  for i = 0 to str_len - sub_len do
+    if String.sub str i sub_len = sub then incr count
+  done ;
+  !count
+
+let rec expected_edges = function
+  | Const _ | Var _ -> 0
+  | Neg e | Exp e | Log e | Sin e | Cos e | Tanh e -> 1 + expected_edges e
+  | Add (l, r) | Sub (l, r) | Mul (l, r) | Div (l, r) ->
+      2 + expected_edges l + expected_edges r
+
+let test_constant _ =
+  let dot = expr_to_dot (Const 1.) in
+  assert_equal 1 (count_occurrences dot "1.")
+
+let test_var _ =
+  let dot = expr_to_dot (Var "x") in
+  assert_equal 1 (count_occurrences dot "\"x\"")
+
+let test_var_shared _ =
+  let dot = expr_to_dot (Add (Var "x", Var "x")) in
+  assert_equal 1 (count_occurrences dot "\"x\"")
+
+let test_unary_edges _ =
+  let ast = Log (Neg (Var "x")) in
+  let dot = expr_to_dot ast in
+  assert_equal (expected_edges ast) (count_occurrences dot "->")
+
+let test_binary_edges _ =
+  let ast = Add (Mul (Var "x", Var "y"), Var "z") in
+  let dot = expr_to_dot ast in
+  assert_equal (expected_edges ast) (count_occurrences dot "->")
+
+let test_edges _ =
+  let ast = Div (Mul (Var "x", Var "y"), Sin (Var "z")) in
+  let dot = expr_to_dot ast in
+  assert_equal (expected_edges ast) (count_occurrences dot "->")
+
+(* let test_operator_labels _ = let dot = expr_to_dot (Add (Var "x", Var "y"))
+   in assert_equal 1 (count_occurrences dot "\"+\"") *)
+
+(* let test_multiple_op_labels _ = let dot = expr_to_dot (Add (Var "x", Add (Var
+   "y", Var "z"))) in assert_equal 2 (count_occurrences dot "\"+\"") *)
+
+let test_write_expr_dot _ =
+  let path = Filename.temp_file "test_dot" ".dot" in
+  let ast = Sub (Var "x", Const 1.0) in
+  let result = write_expr_dot path ast in
+  assert_equal (Ok ()) result ;
+  let channel = open_in path in
+  let contents = really_input_string channel (in_channel_length channel) in
+  close_in channel ;
+  assert_equal (expr_to_dot ast) contents ;
+
+  Sys.remove path
+
+let suite =
+  "graphviz_export property tests"
+  >::: [
+         "constants" >:: test_constant;
+         "variable" >:: test_var;
+         "multiple variable" >:: test_var_shared;
+         "unary op edges" >:: test_unary_edges;
+         "binary op edges" >:: test_binary_edges;
+         "unary and binary edges" >:: test_edges;
+         "writing dot file" >:: test_write_expr_dot;
+       ]
+
+let () = run_test_tt_main suite
